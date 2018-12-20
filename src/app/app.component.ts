325f016eaa8b46cf4aa4de8fa10c30c07212eac8
Manish Singh <20386467+manish7singh@users.noreply.github.com>
2018-12-20 07:30:49 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'va-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'va-game';
+}
